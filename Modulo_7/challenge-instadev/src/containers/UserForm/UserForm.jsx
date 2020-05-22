1a082507b66ff4a6d70c7e111813e0525329e8d8
ElderSam <eldersamuel98@gmail.com>
2020-05-22 16:49:16 -0300

    formulário de cadastro com mensagens

--- Modulo_7/challenge-instadev/src/containers/UserForm/UserForm.jsx
+++ Modulo_7/challenge-instadev/src/containers/UserForm/UserForm.jsx
@@ -1,13 +1,88 @@
 import React, { useState } from 'react';
+import profilePlaceholder from '../../assets/img/instagram-logo.svg';
+import SuccessMessage from '../../components/SuccessMessage';
 
-//import SuccessMessage from '../../components/SuccessMessage';
-
+import '../UserProfile/UserProfile.scss';
 import './UserForm.scss';
 
 const UserForm = () => {
+  const [name, setName] = useState('John Doe');
+  const [username, setUsername] = useState('johndoe');
+  const [email, setEmail] = useState('johndoe@email.com');
+  const [profileImg, setProfileImg] = useState('');
+  const [formStatus, setFormStatus] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormStatus(true);
+  };
+
   return (
-    <React.Fragment>
-    </React.Fragment>
+    <>
+      {formStatus && <SuccessMessage />}
+      <div data-testid="user-form">
+        <section className="profile">
+          <div className="container">
+            <div className="profile-data">
+              <div className="user">
+                <div className="user__thumb">
+                  <img src={profileImg || profilePlaceholder} alt={name} />
+                </div>
+                <p className="user__name">
+                  {name} <span>@{username}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="post__form">
+          <div className="container">
+            <div className="post__form__wrapper">
+              <form onSubmit={handleSubmit} className="post__form">
+                <label htmlFor="name-text-input">Nome</label>
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  id="name-text-input"
+                  type="text"
+                  placeholder="Ex.: Fulano da Silva"
+                  value={name}
+                  required
+                />
+                <label htmlFor="username-text-input">Usuário</label>
+                <input
+                  onChange={(e) => setUsername(e.target.value)}
+                  id="username-text-input"
+                  type="text"
+                  placeholder="Ex.: fulanodasilva"
+                  value={username}
+                  required
+                />
+                <label htmlFor="email-text-input">Email</label>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  id="email-text-input"
+                  type="text"
+                  placeholder="Ex.: fulanodasilva@email.com"
+                  value={email}
+                  required
+                />
+                <label htmlFor="profile-image-text-input">
+                  Url da Imagem de Perfil (use a url da imagem do Linkedin)
+                </label>
+                <input
+                  onChange={(e) => setProfileImg(e.target.value)}
+                  id="profile-image-text-input"
+                  type="text"
+                  placeholder="http://...."
+                  required
+                />
+                <button type="submit">Cadastrar</button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
--- Modulo_7/challenge-instadev/src/routes/NewUserRoute/NewUserRoute.jsx
+++ Modulo_7/challenge-instadev/src/routes/NewUserRoute/NewUserRoute.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-//import UserForm from '../../containers/UserForm';
+import UserForm from '../../containers/UserForm';
 
-const NewUserRoute = () => {
-    return(
-        <h1>NewUserRoute</h1>
+const NewUserRoute = () => (
+    <div data-testid="new-user-route">
+        <UserForm />
+    </div>      
 
-    )
-};
+);
+
 
 export default NewUserRoute;
